d745084252faf45f287c58e4b3807b4cb7e6b819
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-02-05 10:45:41 +0100

    create new import.php to load the whole XAPlatform with a single file include

--- libraries/import.php
+++ libraries/import.php
@@ -1,62 +1,50 @@
 <?php
 /**
- * Bootstrap file for the Joomla Platform.  Including this file into your application will make Joomla
- * Platform libraries available for use.
- *
- * @package    Joomla.Platform
- *
- * @copyright  Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE
- */
+  * @info		$Id$ - $Revision$
+  * @package		$XiveApplication.Platform $
+  * @subpackage	Framework
+  * @check		$Date$ || $Result: devXive AntiMal...OK, no malware found $
+  * @author		$Author$ @ devXive - research and development
+  * @copyright	Copyright (C) 1997 - 2013 devXive - research and development (http://www.devxive.com)
+  * @notes		XAF uses the Gantry Framework, (C) by RocketTheme, LLC (http://www.gantry-framework.com)
+  * @dependency	XAP uses the Joomla! Platform, (C) by Open Source Matters (http://api.joomla.org)
+  * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+  * @assetsLicense	devXive Proprietary Use License (http://www.devxive.com/license)
+  */
+
+// set for no direct access functionality in applications (XAP Restricted Access)
+define('_XAPRA', 1);
 
 // Set the platform root path as a constant if necessary.
-if (!defined('JPATH_PLATFORM'))
+if (!defined('XPATH_PLATFORM'))
 {
-	define('JPATH_PLATFORM', __DIR__);
+	define('XPATH_PLATFORM', __DIR__);
 }
 
-// Detect the native operating system type.
-$os = strtoupper(substr(PHP_OS, 0, 3));
-
-if (!defined('IS_WIN'))
-{
-	define('IS_WIN', ($os === 'WIN') ? true : false);
+// Define legacy directory separator as a constant if not exist
+if(!defined('DS')) {
+	define('DS', DIRECTORY_SEPARATOR);
 }
-if (!defined('IS_UNIX'))
+
+// Import the platform version library if necessary.
+if (!class_exists('XAPlatform'))
 {
-	define('IS_UNIX', (IS_WIN === false) ? true : false);
+	require_once XPATH_PLATFORM . '/xplatform.php';
 }
 
 // Import the platform version library if necessary.
 if (!class_exists('JPlatform'))
 {
-	require_once JPATH_PLATFORM . '/platform.php';
+	// File for the Joomla Platform
+	require_once XPATH_PLATFORM . '/joomla.php';
+	
+	// file for the Joomla Platform [with legacy libraries]
+	// require_once XPATH_PLATFORM . '/joomla.legacy.php';
 }
 
-// Import the library loader if necessary.
-if (!class_exists('JLoader'))
+// Import the xive-application-framework if necessary.
+if (!class_exists('XAPlatform'))
 {
-	require_once JPATH_PLATFORM . '/loader.php';
+	require_once XPATH_PLATFORM . DS . '/xaf.php';
 }
 
-// Make sure that the Joomla Platform has been successfully loaded.
-if (!class_exists('JLoader'))
-{
-	throw new RuntimeException('Joomla Platform not loaded.');
-}
-
-// Setup the autoloaders.
-JLoader::setup();
-
-// Import the base Joomla Platform libraries.
-JLoader::import('joomla.factory');
-
-// Register classes for compatability with PHP 5.3
-if (version_compare(PHP_VERSION, '5.4.0', '<'))
-{
-	JLoader::register('JsonSerializable', JPATH_PLATFORM . '/compat/jsonserializable.php');
-}
-
-// Register classes that don't follow one file per class naming conventions.
-JLoader::register('JText', JPATH_PLATFORM . '/joomla/language/text.php');
-JLoader::register('JRoute', JPATH_PLATFORM . '/joomla/application/route.php');
