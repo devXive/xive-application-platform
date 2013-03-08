fc3e1b65635d98dc33e121abd7ae464c2f59dbb8
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-03-08 01:29:21 +0100

    adding full BS 222

---
+++ media/bootstrap/js/application.js
@@ -0,0 +1,3 @@
+jQuery('.xaf-tip').tooltip();
+jQuery('.xaf-pophover').popover({'html':true, 'placement':'top', 'trigger':'hover'});
+jQuery('.xaf-pophoverLarger').popover({'template':'<div class="popover"><div class="arrow"></div><div class="popover-inner larger"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div></div></div>', 'html':true, 'placement':'top', 'trigger':'hover'});
