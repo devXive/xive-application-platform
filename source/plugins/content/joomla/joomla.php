65469048f58d00b076a9b26929d6bcfe5eebdb93
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-05-12 19:01:00 +0200

    remove newsfeed

--- source/plugins/content/joomla/joomla.php
+++ source/plugins/content/joomla/joomla.php
@@ -111,8 +111,7 @@ class PlgContentJoomla extends JPlugin
 
 		$tableInfo = array(
 			'com_contact' => array('table_name' => '#__contact_details'),
-			'com_content' => array('table_name' => '#__content'),
-			'com_newsfeeds' => array('table_name' => '#__newsfeeds')
+			'com_content' => array('table_name' => '#__content')
 		);
 
 		// Now check to see if this is a known core extension
