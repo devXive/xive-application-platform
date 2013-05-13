e91ecbfed24f85cc0486a00975e69e1fc5e651e5
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-05-13 04:25:38 +0200

    remove frontend module footer
    Closes #65

--- source/administrator/components/com_admin/script.php
+++ source/administrator/components/com_admin/script.php
@@ -101,7 +101,6 @@ class JoomlaInstallerScript
 		// Site
 		$extensions[] = array('module', 'mod_breadcrumbs', '', 0);
 		$extensions[] = array('module', 'mod_custom', '', 0);
-		$extensions[] = array('module', 'mod_footer', '', 0);
 		$extensions[] = array('module', 'mod_login', '', 0);
 		$extensions[] = array('module', 'mod_menu', '', 0);
 		$extensions[] = array('module', 'mod_search', '', 0);
--- source/administrator/components/com_admin/sql/updates/mysql/3.0.0.sql
+++ source/administrator/components/com_admin/sql/updates/mysql/3.0.0.sql
@@ -13,7 +13,6 @@ ALTER TABLE `#__updates` DROP COLUMN `categoryid`;
 
 UPDATE `#__extensions` SET protected = 0 WHERE
 `name` = 'com_search' OR
-`name` = 'mod_footer' OR
 `name` = 'plg_content_pagebreak' OR
 `name` = 'plg_content_pagenavigation' OR
 `name` = 'plg_content_vote' OR
