e8faa1001eb290dd79accd9c9ac81712e7d20627
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-05-22 04:30:48 +0200

    added nawala call to framework

--- source/includes/framework.php
+++ source/includes/framework.php
@@ -98,3 +98,9 @@ if (JDEBUG)
 {
 	$_PROFILER = JProfiler::getInstance('Application');
 }
+
+// set for no direct access functionality in applications (NFW Restricted Access)
+define('_NFWRA', 1);
+
+// Nawala Framework include
+require_once JPATH_LIBRARIES.'/nawala.php';
