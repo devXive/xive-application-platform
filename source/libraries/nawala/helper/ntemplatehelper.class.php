465035bb5015047297b1cc66a044ddc03cf6f224
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-05-22 06:05:08 +0200

    added ntemplatehelper class

---
+++ source/libraries/nawala/helper/ntemplatehelper.class.php
@@ -0,0 +1,37 @@
+<?php
+/**
+  * @info		$Id$ - $Revision$
+  * @package		$Nawala.Framework $
+  * @subpackage	Framework
+  * @check		$Date$ || $Result: devXive AntiMal...OK, nothing found $
+  * @author		$Author$ @ devXive - research and development <support@devxive.com>
+  * @copyright	Copyright (C) 1997 - 2013 devXive - research and development (http://www.devxive.com)
+  * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+  * @assetsLicense	devXive Proprietary Use License (http://www.devxive.com/license)
+  */
+
+// no direct access
+defined('_NFWRA') or die;
+
+class NTemplateHelper {
+
+	/**
+	 * @param  string $path      the path to the folder
+	 * @param  string $filename  the filename
+	 *
+	 */
+	public function loadBottomHead($path, $filename) {
+//		$doc = JFactory::getDocument;
+//		$doc->addStyle($path, $filename);
+	}
+
+	/**
+	 * @param  string $path      the path to the folder
+	 * @param  string $filename  the filename
+	 *
+	 */
+	public function loadBottomBody($path, $filename) {
+//		$doc = JFactory::getDocument;
+//		$doc->addScript($path, $filename);
+	}
+}
