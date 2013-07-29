1ad648c4657aab9c979b13800a069c1a6b31e7c7
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-07-29 14:44:48 +0200

    removed nawala framework from core framework file

--- source/administrator/includes/framework.php
+++ source/administrator/includes/framework.php
@@ -91,9 +91,3 @@ if (JDEBUG)
 {
 	$_PROFILER = JProfiler::getInstance('Application');
 }
-
-// set for no direct access functionality in applications (NFW Restricted Access)
-define('_NFWRA', 1);
-
-// Nawala Framework include
-require_once JPATH_LIBRARIES.'/nawala.php';
